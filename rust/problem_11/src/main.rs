69c53b309b0fd74db0eafee7e44e3dd1d4a92b3b
grdw <gerard@wetransfer.com>
2020-03-05 09:57:18 +0100

    Add solution to problem 11

---
+++ rust/problem_11/src/main.rs
@@ -0,0 +1,113 @@
+fn max(num: i32, other_num: i32) -> i32 {
+    if num > other_num {
+        num
+    } else {
+        other_num
+    }
+}
+
+fn max_product_for_grid(grid: Vec<Vec<i32>>) -> i32 {
+    let grid_size = grid.len();
+    let spots: usize = 3; // How far do I have to look?
+    let mut highest_factor: i32 = 0;
+
+    for y in 0..grid_size {
+        for x in 0..grid_size {
+            let can_look_right: bool = (x + spots) < grid_size;
+            let can_look_down: bool = (y + spots) < grid_size;
+
+            if can_look_right && can_look_down {
+                highest_factor = max(
+                    grid[y][x] * grid[y + 1][x + 1] * grid[y + 2][x + 2],
+                    highest_factor
+                );
+            }
+
+            if can_look_right {
+                highest_factor = max(
+                    grid[y][x] * grid[y][x + 1] * grid[y][x + 2],
+                    highest_factor
+                );
+            }
+
+            if can_look_down {
+                highest_factor = max(
+                    grid[y][x] * grid[y + 1][x] * grid[y + 2][x],
+                    highest_factor
+                )
+            }
+        }
+    }
+
+    highest_factor
+}
+
+#[test]
+fn greatest_product_small_grid() {
+    let diagonal_grid = vec![
+        vec![1, 1, 1, 1, 1],
+        vec![1, 10, 1, 1, 1],
+        vec![1, 1, 10, 1, 1],
+        vec![1, 1, 1, 1, 1],
+       	vec![1, 1, 1, 1, 1]
+    ];
+
+    let horizontal_grid = vec![
+        vec![1, 10, 20, 1, 1],
+        vec![1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1]
+    ];
+
+    let vertical_grid = vec![
+        vec![1, 10, 1, 1, 1],
+        vec![1, 30, 1, 1, 1],
+        vec![1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1]
+    ];
+
+    let random_grid = vec![
+        vec![1, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 1, 1, 1],
+        vec![1, 1, 1, 1, 20, 1, 1],
+        vec![1, 1, 1, 1, 1, 10, 1],
+        vec![1, 1, 1, 1, 1, 1, 1]
+    ];
+
+    assert_eq!(max_product_for_grid(diagonal_grid), 100);
+    assert_eq!(max_product_for_grid(horizontal_grid), 200);
+    assert_eq!(max_product_for_grid(vertical_grid), 300);
+    assert_eq!(max_product_for_grid(random_grid), 200);
+}
+
+#[test]
+fn greatest_product_the_real_thing() {
+    let grid = vec![
+      vec![08, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 08],
+      vec![49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00],
+      vec![81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65],
+      vec![52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91],
+      vec![22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80],
+      vec![24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50],
+      vec![32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70],
+      vec![67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 08, 40, 91, 66, 49, 94, 21],
+      vec![24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72],
+      vec![21, 36, 23, 09, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95],
+      vec![78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 09, 53, 56, 92],
+      vec![16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57],
+      vec![86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
+      vec![19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40],
+      vec![04, 52, 08, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
+      vec![88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69],
+      vec![04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 08, 46, 29, 32, 40, 62, 76, 36],
+      vec![20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16],
+      vec![20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54],
+      vec![01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48]
+    ];
+
+    assert_eq!(max_product_for_grid(grid), 776776);
+}
