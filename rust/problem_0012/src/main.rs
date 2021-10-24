712482158fd149fa83932d41f7ddff562ec72a8d
grdw <gerard@grdw.nl>
2021-10-24 17:14:15 +0200

    Answer for euler 12

---
+++ rust/problem_0012/src/main.rs
@@ -0,0 +1,52 @@
+// https://projecteuler.net/problem=12
+fn main() {
+    println!("Hello, world!");
+}
+
+fn num_factors(n: i64) -> i64 {
+    if n == 1 {
+        return n;
+    }
+
+    let t = (n as f64).sqrt() as i64;
+    let mut d = 2; // You always have 2 divisors, 1 and yourself
+
+    for i in 2..=t {
+        if n % i == 0 {
+            d += 2
+        }
+    }
+
+    d
+}
+
+fn problem_12(max_divs: i64) -> i64 {
+    let mut i = 1;
+    let mut j = 1;
+
+    loop {
+        i += 1;
+        j += i;
+
+        let o = num_factors(j);
+
+        if o > max_divs {
+            break j
+        }
+    }
+}
+
+#[test]
+fn test_triangle_five_hundred() {
+    assert_eq!(problem_12(5), 28);
+    assert_eq!(problem_12(500), 76576500);
+}
+
+#[test]
+fn test_num_factors() {
+    assert_eq!(num_factors(60), 12);
+    assert_eq!(num_factors(1), 1);
+    assert_eq!(num_factors(3), 2);
+    assert_eq!(num_factors(6), 4);
+    assert_eq!(num_factors(10), 4);
+}
