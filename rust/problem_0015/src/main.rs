59f7fbbea0b9524979d6ef205686df3d2c210784
grdw <gerard@grdw.nl>
2021-10-25 16:14:54 +0200

    Add solution to euler 15

---
+++ rust/problem_0015/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("Hello, sworld!");
+}
+
+fn problem_15(mut k: u128) -> u128 {
+    let mut n = k * 2;
+    let mut o = 1;
+    let mut p = 1;
+
+    while k > 0 {
+        o *= n;
+        p *= k;
+        n -= 1;
+        k -= 1
+    }
+
+    o / p
+}
+
+
+#[test]
+fn test_problem_15() {
+    assert_eq!(problem_15(2), 6);
+    assert_eq!(problem_15(3), 20);
+    assert_eq!(problem_15(20), 137846528820)
+}
