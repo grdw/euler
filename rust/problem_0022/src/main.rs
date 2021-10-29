bf62a65d8981f27796b60892099a7c381f5a95c0
grdw <gerard@grdw.nl>
2021-10-29 20:15:58 +0200

    Small bits of cleanup

--- rust/problem_0022/src/main.rs
+++ rust/problem_0022/src/main.rs
@@ -4,31 +4,30 @@ fn main() {
 
 use std::fs;
 
+fn alphabet_value(name: &str) -> u32 {
+    name
+        .chars()
+        .map(|c| {
+            match (b'A'..=b'Z').position(|l| l as char == c) {
+                Some(n) => (n + 1) as u32,
+                None => 0
+            }
+        })
+        .sum()
+}
+
 fn problem_22() -> u32 {
     let contents = fs::read_to_string("p022_names.txt")
                       .unwrap_or("".to_string());
 
     let mut names: Vec<&str> = contents.split(",").collect();
-    let alphabet: Vec<char> = (b'A'..=b'Z').map(|n| n as char).collect();
 
     names.sort();
 
     names
         .iter()
         .enumerate()
-        .map(|(i, name)| {
-            let alphabet_value: u32 = name
-                .chars()
-                .map(|c| {
-                    match alphabet.iter().position(|&l| l == c) {
-                        Some(n) => (n + 1) as u32,
-                        None => 0
-                    }
-                })
-                .sum();
-
-            alphabet_value * (i as u32 + 1)
-        })
+        .map(|(i, name)| alphabet_value(name) * (i as u32 + 1))
         .sum()
 }
 
