98f6c54179b796a7013ea97d01c1d74136f2143d
grdw <gerard@grdw.nl>
2021-10-31 14:23:26 +0100

    add some notes for next_perm

--- rust/problem_0024/src/main.rs
+++ rust/problem_0024/src/main.rs
@@ -3,23 +3,24 @@ fn main() {
 }
 
 fn next_perm(res: &mut Vec<u8>) {
-    let mut i = res.len() - 1;
-    while res[i - 1] >= res[i] {
-        i -= 1
+    // Assume res = [0,2,1]
+    let mut i = res.len() - 1; // this becomes 2
+    while res[i - 1] >= res[i] { // is 2 >= 1 YES!
+        i -= 1 // this becomes 1
     }
 
-    let mut j = res.len();
-    while res[j - 1] <= res[i - 1] {
+    let mut j = res.len(); // this becomes 3
+    while res[j - 1] <= res[i - 1] { // is 1 <= 0 NO
         j -= 1;
     }
 
-    res.swap(i - 1, j - 1);
+    res.swap(i - 1, j - 1); // SWAP 0 WITH 2 => [1,2,0]
 
-    i += 1;
-    j = res.len();
+    i += 1; // i becomes 2
+    j = res.len(); // j becomes 3
 
-    while i < j {
-        res.swap(i - 1, j - 1);
+    while i < j { // is 2 < 3, YEP
+        res.swap(i - 1, j - 1); // SWAP 1 WITH 2 , STOP => [1,0,2]
         i += 1;
         j -= 1;
     }
