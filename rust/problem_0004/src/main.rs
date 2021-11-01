8716c21d965ee44574f888bdeb40d5b85368fe8f
grdw <gerard@wetransfer.com>
2021-11-01 12:47:11 +0100

    Add answer for euler 4

--- rust/problem_0004/src/main.rs
+++ rust/problem_0004/src/main.rs
@@ -1,40 +1,12 @@
-fn is_palindrome(x: i32) -> bool {
+fn is_palindrome(x: u32) -> bool {
     let string = x.to_string();
     let len = string.len() - 1;
+    let end = string.len() / 2;
 
-    if len == 0 {
-        return true
-    }
-
-    let end = (len + 1) / 2;
-    let r: Vec<usize> = (0..end).collect();
-
-    r.iter().all(|x| {
-        let l = string.chars().nth(*x);
-        let r = string.chars().nth(len - *x);
-        l == r
-    })
-}
-
-fn max_palindrome_digits(digits: u32) -> i32 {
-    let mut highest_pal = 0;
-    let mut max_number = 10_i32.pow(digits) - 1;
-
-    'outer: while max_number > 0 {
-        let mut inner_max = max_number;
-
-        while inner_max > 0 {
-            let factor = max_number * inner_max;
-
-            if is_palindrome(factor) {
-                highest_pal = factor;
-                break 'outer;
-            }
-            inner_max -= 1
-        }
-        max_number -= 1
-    }
-    highest_pal
+    string[0..end]
+        .chars()
+        .enumerate()
+        .all(|(i,n)| n == string.chars().nth(len - i).unwrap())
 }
 
 #[test]
@@ -47,9 +19,20 @@ fn is_palindrome_test() {
     assert_eq!(is_palindrome(10101), true);
 }
 
+fn problem_4() -> u32 {
+    let mut max = 0;
+    for i in 100..=999 {
+        for j in 100..=999 {
+            let product = i * j;
+            if product > max && is_palindrome(product) {
+                max = product;
+            }
+        }
+    }
+    max
+}
+
 #[test]
-fn palindrome_numbers_test() {
-    assert_eq!(max_palindrome_digits(2), 9009);
-    assert_eq!(max_palindrome_digits(3), 90909);
-    assert_eq!(max_palindrome_digits(4), 99000099);
+fn test_problem_4() {
+    assert_eq!(problem_4(), 906609)
 }
