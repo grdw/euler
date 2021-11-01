015eae5d42b0fdb0cd9261cbaac0b371dbf221a2
grdw <gerard@wetransfer.com>
2021-11-01 13:38:19 +0100

    Add solution to 6

---
+++ rust/problem_0006/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+fn problem_6() -> u128 {
+    let mut sum: u128 = 0;
+    let mut n_sum: u128 = 0;
+    for n in 1..100 {
+        sum += (n as u128).pow(2);
+        n_sum += n;
+    }
+    n_sum.pow(2) - sum
+}
+
+#[test]
+fn test_problem_6() {
+    assert_eq!(problem_6(), 24174150);
+}
