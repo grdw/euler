7f059b035549b7b25f723d90ecc473b90a8217dc
grdw <gerard@wetransfer.com>
2021-11-01 13:28:49 +0100

    Add p5

---
+++ rust/problem_0005/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn problem_5() -> u64 {
+    let mut start = 20;
+    loop {
+        if (1..=20).all(|x| start % x == 0) {
+            break start
+        }
+        start += 20
+    }
+}
+
+#[test]
+fn test_problem_5() {
+    assert_eq!(problem_5(), 0);
+}
