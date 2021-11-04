4eff6082dce71f4f252493f65129a7c49f52f16d
grdw <gerard@grdw.nl>
2021-11-04 21:37:23 +0100

    Add the correct answer for 29

--- rust/problem_0029/src/main.rs
+++ rust/problem_0029/src/main.rs
@@ -2,13 +2,13 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn is_prime(number: u64) -> bool {
+fn is_prime(number: u8) -> bool {
     if number < 2 {
         return false
     }
 
     let mut is_prime: bool = true;
-    let end = (number as f64).sqrt().floor() as u64;
+    let end = (number as f64).sqrt().floor() as u8;
 
     for i in 2..end+1 {
         if number % i == 0 {
@@ -19,10 +19,10 @@ fn is_prime(number: u64) -> bool {
     is_prime
 }
 
-fn prime_factors(mut number: u64) -> Vec<u64> {
-    let mut factors: Vec<u64> = vec![];
-    let mut factor: u64 = 2;
-    let end = (number as f64).sqrt().floor() as u64;
+fn prime_factors(mut number: u8) -> Vec<u8> {
+    let mut factors: Vec<u8> = vec![];
+    let mut factor: u8 = 2;
+    let end = (number as f64).sqrt().floor() as u8;
 
     while number > 1 {
         if is_prime(factor) && number % factor == 0 {
@@ -44,28 +44,32 @@ fn test_prime_factors() {
     assert_eq!(prime_factors(10), vec![2, 5]);
     assert_eq!(prime_factors(99), vec![3, 3, 11]);
     assert_eq!(prime_factors(100), vec![2, 2, 5, 5]);
-    assert_eq!(prime_factors(256), vec![2, 2, 2, 2, 2, 2, 2, 2]);
-    assert_eq!(prime_factors(3125), vec![5, 5, 5, 5, 5]);
 }
 
-fn problem_29(max: u64) -> u64 {
-    let mut size:u64 = 0;
-    for i in 2..=max {
-        for j in 2..=max {
-            let total = vec![i; j as usize];
-            println!("{:?}", total);
+fn problem_29(max: u16) -> u16 {
+    let mut totals: Vec<String> = vec![];
+    for a in 2..=max {
+        for b in 2..=max {
+            let primes = prime_factors(a as u8);
+            let mut string = String::from("");
+
+            for i in 0..primes.len() {
+                let sub_string = primes[i].to_string().repeat(b as usize);
+                string.push_str(&sub_string);
+            }
+
+            if !totals.contains(&string) {
+                totals.push(string);
+            }
         }
     }
-    0
+    println!("{:?}", totals);
+    totals.len() as u16
 }
 
 #[test]
 fn test_problem_29() {
-    for i in 5..7 {
-        println!("{:?}", problem_29(i));
-
-    }
-    //assert_eq!(problem_29(5), 15);
-    //assert_eq!(problem_29(6), 23);
-    //assert_eq!(problem_29(10), 69);
+    assert_eq!(problem_29(5), 15);
+    assert_eq!(problem_29(6), 23);
+    assert_eq!(problem_29(100), 9183);
 }
