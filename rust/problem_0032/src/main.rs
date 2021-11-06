343045e2c5e450a808f31b17a25e902bd13e51b7
grdw <gerard@grdw.nl>
2021-11-06 23:15:09 +0100

    Simplify the code

--- rust/problem_0032/src/main.rs
+++ rust/problem_0032/src/main.rs
@@ -3,16 +3,9 @@ fn valid_divisors(i: u64, d1: u64, d2: u64) -> bool {
     s.push_str(&d1.to_string());
     s.push_str(&d2.to_string());
 
-    if s.contains('0') {
-        return false
-    }
-
-    let vector: Vec<char> = s.chars().collect();
-    let mut y = vector.clone();
-    y.sort();
-    y.dedup();
-
-    vector.len() == y.len()
+    let mut vector: Vec<char> = s.chars().collect();
+    vector.sort();
+    vector == vec!['1','2','3','4','5','6','7','8','9']
 }
 
 #[test]
@@ -40,40 +33,25 @@ fn test_common_divisors() {
     assert_eq!(divisors(60), vec![]);
     assert_eq!(divisors(7254), vec![(39, 186)]);
     assert_eq!(divisors(64), vec![]);
+    assert_eq!(divisors(6), vec![]);
 }
 
 fn problem_32() -> u64 {
     let mut n = 98765;
-    let mut all_ns: Vec<(u64, u64, u64)> = vec![];
+    let mut all_ns: Vec<u64> = vec![];
 
     while n > 0 {
         let d = divisors(n);
 
         if !d.is_empty() {
-            for (d1, d2) in &d {
-                all_ns.push((n, *d1, *d2));
-            }
+            all_ns.push(n);
         }
         n -= 1;
     }
 
-    let mut all_factors = vec![];
-    for (sum, f1, f2) in all_ns {
-        let mut string = sum.to_string();
-        string.push_str(&f1.to_string());
-        string.push_str(&f2.to_string());
-
-        let mut sorted: Vec<char> = string.chars().collect();
-        sorted.sort();
-
-        if sorted == vec!['1','2','3','4','5','6','7','8','9'] {
-            all_factors.push(sum);
-        }
-    }
-
-    all_factors.sort();
-    all_factors.dedup();
-    all_factors.iter().sum()
+    all_ns.sort();
+    all_ns.dedup();
+    all_ns.iter().sum()
 }
 
 #[test]
