9db7a7df1405790ec4dba553af730d8655726ead
grdw <gerard@grdw.nl>
2021-11-09 20:11:39 +0100

    Simplify is_truncatable_prime a bit

--- rust/problem_0037/src/main.rs
+++ rust/problem_0037/src/main.rs
@@ -20,20 +20,27 @@ fn is_truncatable_prime(number: i64) -> bool {
         return false;
     }
 
-    let mut truncatable = true;
     let n = number.to_string();
 
-    for i in 0..n.len() {
+    (0..n.len()).all(|i| {
         let r_slice: i64 = n[0..i+1].parse().unwrap_or(-1);
         let l_slice: i64 = n[i..n.len()].parse().unwrap_or(-1);
 
-        if !is_prime(r_slice) || !is_prime(l_slice) {
-            truncatable = false;
-            break;
-        }
-    }
+        is_prime(r_slice) && is_prime(l_slice)
+    })
+}
 
-    truncatable
+#[test]
+fn is_truncatable_test() {
+    assert_eq!(is_truncatable_prime(4), false);
+    assert_eq!(is_truncatable_prime(2), false);
+    assert_eq!(is_truncatable_prime(3), false);
+    assert_eq!(is_truncatable_prime(5), false);
+    assert_eq!(is_truncatable_prime(7), false);
+    assert_eq!(is_truncatable_prime(11), false);
+    assert_eq!(is_truncatable_prime(233), false);
+    assert_eq!(is_truncatable_prime(4733), false);
+    assert_eq!(is_truncatable_prime(3797), true);
 }
 
 const MAX_TRUNCATABLES: i32 = 11;
@@ -57,19 +64,6 @@ fn truncatable_primes() -> i64 {
     }
 
     sum
-}
-
-#[test]
-fn is_truncatable_test() {
-    assert_eq!(is_truncatable_prime(4), false);
-    assert_eq!(is_truncatable_prime(2), false);
-    assert_eq!(is_truncatable_prime(3), false);
-    assert_eq!(is_truncatable_prime(5), false);
-    assert_eq!(is_truncatable_prime(7), false);
-    assert_eq!(is_truncatable_prime(11), false);
-    assert_eq!(is_truncatable_prime(233), false);
-    assert_eq!(is_truncatable_prime(4733), false);
-    assert_eq!(is_truncatable_prime(3797), true);
 }
 
 #[test]
