4054909c6d8f837bde518fe385c34f3394414fcc
grdw <gerard@grdw.nl>
2021-11-10 18:20:13 +0100

    Simplify answer

--- rust/problem_0038/src/main.rs
+++ rust/problem_0038/src/main.rs
@@ -1,4 +1,4 @@
-fn multiply_concatenate(n: u64) -> Option<String> {
+fn multiply_concatenate(n: u64) -> String {
     let mut multiplier = 1;
     let mut total = String::from("");
     let pandigital = vec!['1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -17,13 +17,13 @@ fn multiply_concatenate(n: u64) -> Option<String> {
             });
 
         if duplicates {
-            break None
+            break String::from("")
         }
 
         chars.sort();
 
         if chars == pandigital {
-            break Some(total)
+            break total
         }
 
         multiplier += 1;
@@ -32,24 +32,18 @@ fn multiply_concatenate(n: u64) -> Option<String> {
 
 #[test]
 fn test_multiply_concatenate() {
-    assert_eq!(multiply_concatenate(192), Some(String::from("192384576")));
-    assert_eq!(multiply_concatenate(1), Some(String::from("123456789")));
-    assert_eq!(multiply_concatenate(2), None)
+    assert_eq!(multiply_concatenate(192), String::from("192384576"));
+    assert_eq!(multiply_concatenate(1), String::from("123456789"));
+    assert_eq!(multiply_concatenate(2), String::from(""))
 }
 
 fn problem_38() -> String {
-    let mut max = 1;
     let mut result = String::from("");
 
     for n in 1..=99_999 {
-        match multiply_concatenate(n) {
-            Some(t) => {
-                if n > max {
-                    result = t;
-                    max = n;
-                }
-            },
-            None => {}
+        let t = multiply_concatenate(n);
+        if !t.is_empty() {
+            result = t;
         }
     }
     result
