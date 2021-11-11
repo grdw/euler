299ab820cb83bf77709c0dea6dd8c21874791c5b
grdw <gerard@grdw.nl>
2021-11-11 14:20:49 +0100

    Fix problem 39

--- rust/problem_0039/src/main.rs
+++ rust/problem_0039/src/main.rs
@@ -25,10 +25,10 @@ fn problem_39() -> u64 {
     let max_r: u64 = max - 2;
     let mut triplets = vec![0; (max + 1) as usize];
     let mut max_index = 0;
-    let mut max = 0;
+    let mut max_f = 0;
 
     for a in 1..=max_r {
-        for b in a..=max_r {
+        for b in 1..=max_r {
             if let Some(c) = calculate_c(a, b) {
                 let p = a + b + c;
 
@@ -40,8 +40,8 @@ fn problem_39() -> u64 {
     }
 
     for (i, f) in triplets.iter().enumerate() {
-        if *f > max {
-            max = *f;
+        if *f > max_f {
+            max_f = *f;
             max_index = i;
         }
     }
