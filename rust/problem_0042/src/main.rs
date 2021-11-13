33c6c38be458e839e5a0e82b4e705d018752e4bf
grdw <gerard@grdw.nl>
2021-11-13 20:54:38 +0100

    Use the perfect square method

--- rust/problem_0042/src/main.rs
+++ rust/problem_0042/src/main.rs
@@ -12,51 +12,24 @@ fn alphabet_value(name: &str) -> u32 {
         .sum()
 }
 
-fn triangle_number_generator() -> Vec<u32> {
-    let max = 364;
-    let mut start = 1;
-    let mut result = vec![];
-    loop {
-        let triangle = (
-            (0.5 * start as f32) * (start as f32 + 1.0)
-        ) as u32;
+fn is_triangle(n: &str) -> bool {
+    let word_value = alphabet_value(n) as f64;
+    let square = ((8.0 * word_value + 1.0).sqrt() - 1.0) / 2.0;
 
-        result.push(triangle);
-        start += 1;
-
-        if triangle > max {
-            break result
-        }
-    }
-}
-
-#[test]
-fn test_triangle_number_generator() {
-    let list = triangle_number_generator();
-    assert_eq!(list.contains(&1), true);
-    assert_eq!(list.contains(&2), false);
-}
-
-fn is_triangle(n: &str, list: &Vec<u32>) -> bool {
-    let word_value = alphabet_value(n);
-
-    list.contains(&word_value)
+    square.fract() == 0.0
 }
 
 #[test]
 fn test_is_triangle() {
-    let list = triangle_number_generator();
-    assert_eq!(is_triangle("SKY", &list), true);
-    assert_eq!(is_triangle("ZZZZZZZZZZZZZZ", &list), false);
+    assert_eq!(is_triangle("SKY"), true);
+    assert_eq!(is_triangle("ZZZZZZZZZZZZZZ"), false);
 }
 
 fn problem_42() -> usize {
     let contents = fs::read_to_string("p042_words.txt")
                       .unwrap_or("".to_string());
 
-    let list = triangle_number_generator();
-
-    contents.split(",").filter(|t| is_triangle(t, &list)).count()
+    contents.split(",").filter(|t| is_triangle(t)).count()
 }
 
 #[test]
