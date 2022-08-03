2b4bfd84fbeea50b3cb9bb54a955850c041f4b84
Gerard <gerard@wetransfer.com>
2022-08-03 12:12:18 +0200

    Add a diagonal function

--- problem_0058/main.go
+++ problem_0058/main.go
@@ -23,6 +23,25 @@ func isPrime(num int) bool {
     return isPrime
 }
 
+func diagonal(size int) float64 {
+    var start int = 1
+    var total int = 1;
+    var primes int = 0;
+
+    for factor := 1; factor < size; factor++ {
+        for k := 0; k < 4; k++ {
+            start += (factor * 2)
+            total++
+
+            if isPrime(start) {
+                primes++
+            }
+        }
+    }
+
+    return float64(primes) / float64(total)
+}
+
 func main() {
     fmt.Println("Hello world.")
 }
