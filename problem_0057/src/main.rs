42749eec6e386533996e26721b4873899d1ef6ea
Gerard <gerard@wetransfer.com>
2022-09-09 16:46:07 +0200

    Write the main Fraction code
    Stolen from: https://rosettacode.org/wiki/Continued_fraction\#Go

--- problem_0057/src/main.rs
+++ problem_0057/src/main.rs
@@ -2,21 +2,35 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn expand_sqrt_of_2(cycles: i32) -> (i32, f32) {
-    let mut start_n = 1;
-    let mut start_d = 2;
-    for _ in 0..cycles {
-        start_d += 0.5;
-    };
+#[derive(Debug, PartialEq)]
+struct Fraction(i64, i64);
 
-    (0, 0.0)
+fn sqrt_of_2(n: usize) -> Vec<Fraction> {
+    let mut list = vec![];
+
+    for _ in 0..n {
+        list.push(Fraction(2, 1))
+    }
+
+    list[0].0 = 1;
+    return list
+}
+
+fn real(l: Vec<Fraction>) -> f64 {
+    let mut t: f64 = 0.0;
+
+    for i in (1..(l.len() - 1)).rev() {
+        t = l[i].1 as f64 / (l[i].0 as f64 + t);
+    }
+
+    return t + l[0].0 as f64
 }
 
 #[test]
 fn test_expand_sqrt_of_2() {
-    assert_eq!(expand_sqrt_of_2(2), (1, 2.0));
-    //assert_eq!(expand_sqrt_of_2(1), 1.5);
-    //assert_eq!(expand_sqrt_of_2(2), 1.4);
-    //assert_eq!(expand_sqrt_of_2(3), 1.41666);
-    //assert_eq!(expand_sqrt_of_2(4), 1.41379);
+    assert_eq!(real(sqrt_of_2(20)), 1.414213562373087);
+    assert_eq!(real(sqrt_of_2(4)), 1.4);
+    assert_eq!(real(sqrt_of_2(3)), 1.5);
+    assert_eq!(real(sqrt_of_2(2)), 1.0);
+    assert_eq!(real(sqrt_of_2(1)), 1.0);
 }
