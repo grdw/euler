9468d221fae9138d16edf406df34261f0125a26b
Gerard <gerard@wetransfer.com>
2024-05-30 15:22:25 +0200

    Cleaning up p67

--- problem_0067/src/main.rs
+++ problem_0067/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::cmp;
 
 fn main() {
     let t = fs::read_to_string("0067_triangle.txt").unwrap();
@@ -9,18 +10,13 @@ fn main() {
         }).collect();
 
     while v.len() > 1 {
-        while let Some(row) = v.pop() {
+        if let Some(row) = v.pop() {
             let index = v.len() - 1;
             let l = v[index].len();
-
             for j in 0..l {
                 let m = row.get(j).unwrap_or(&0) + v[index][j];
                 let n = row.get(j + 1).unwrap_or(&0) + v[index][j];
-                if m > n {
-                    v[index][j] = m;
-                } else {
-                    v[index][j] = n;
-                }
+                v[index][j] = cmp::max(m, n);
             }
         }
     }
