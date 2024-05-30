309f5500698a4ecf107909cef719f4f27de95f6c
Gerard <gerard@wetransfer.com>
2024-05-30 15:26:58 +0200

    Add some blank lines for readability

--- problem_0067/src/main.rs
+++ problem_0067/src/main.rs
@@ -13,12 +13,15 @@ fn main() {
         if let Some(row) = v.pop() {
             let index = v.len() - 1;
             let l = v[index].len();
+
             for j in 0..l {
                 let m = row.get(j).unwrap_or(&0) + v[index][j];
                 let n = row.get(j + 1).unwrap_or(&0) + v[index][j];
+
                 v[index][j] = cmp::max(m, n);
             }
         }
     }
+
     println!("The answer is: {}", v[0][0]);
 }
