05a1a8022f8f83bfabcad119c4d1a3236fd46b1d
Gerard <gerard@wetransfer.com>
2024-06-03 15:41:43 +0200

    Have passable examples

---
+++ problem_0088/src/main.rs
@@ -0,0 +1,56 @@
+use std::collections::HashSet;
+
+fn main() {
+    println!("Answer: {}", sum_group(2, 12000));
+}
+
+fn sum_group(min: u32, max: u32) -> u32 {
+    let mut answer = 0;
+    let mut s: HashSet<u32> = HashSet::new();
+
+    for n in min..=max {
+        let p = sum_l(n);
+        s.insert(p);
+    }
+
+    for v in &s {
+        answer += v;
+    }
+
+    return answer
+}
+
+fn sum_l(n: u32) -> u32 {
+    let mut counter = 10_u32.pow(n - 1);
+    let mut answer = u32::MAX;
+
+    loop {
+        counter += 1;
+
+        let num_length = ((counter as f64).log10() + 1.0).floor() as u32;
+        if num_length == n + 1 {
+            break;
+        }
+
+        let mut v: Vec<u32> = vec![];
+        for i in 0..num_length {
+            let n = counter % (10_u32.pow(i + 1)) / 10_u32.pow(i);
+            v.push(n);
+        }
+        let pr: u32 = v.iter().product();
+        let sm: u32 = v.iter().sum();
+
+        if pr == sm && pr < answer {
+            answer = pr;
+        }
+    }
+
+    return answer
+}
+
+#[test]
+fn test_sum_group() {
+    assert_eq!(sum_group(2, 2), 4);
+    assert_eq!(sum_group(2, 6), 30);
+    //assert_eq!(sum_group(2, 12), 61);
+}
