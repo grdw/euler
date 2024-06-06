88d26b52ab6087e92166069f9dce440d5ef1b4c7
Gerard <gerard@wetransfer.com>
2024-06-06 11:29:18 +0200

    Some refactoring

--- problem_0088/src/main.rs
+++ problem_0088/src/main.rs
@@ -7,24 +7,25 @@ fn main() {
 fn get_multiplication_partitions(n: u64, k: u64) -> bool {
     let mut found = false;
 
-    fn partitions(m: u64, n: u64, start: u64, k: u64, s: u64, p: u64, t: &mut bool) {
+    fn partitions(n: u64, start: u64, k: u64, s: u64, p: u64, t: &mut bool) {
         for i in start..=n {
             if n % i == 0 && i > 1 {
                 let pp = p * i;
                 let ss = s + i + k;
 
-                if ss == pp && m == pp {
+                if ss == pp {
                     *t = true;
+                    break;
                 }
 
                 if k > 0 {
-                    partitions(m, n / i, i, k - 1, s + i, p * i, t);
+                    partitions(n / i, i, k - 1, s + i, p * i, t);
                 }
             }
         }
     }
 
-    partitions(n, n, 2, k - 1, 0, 1, &mut found);
+    partitions(n, 2, k - 1, 0, 1, &mut found);
     return found
 }
 
@@ -34,6 +35,7 @@ fn test_multiplication_partitions() {
     assert_eq!(get_multiplication_partitions(4, 2), true);
     assert_eq!(get_multiplication_partitions(12, 6), true);
     assert_eq!(get_multiplication_partitions(8, 5), true);
+    assert_eq!(get_multiplication_partitions(12096, 12000), true);
 }
 
 fn sum_group(min: u64, max: u64) -> u64 {
@@ -42,18 +44,11 @@ fn sum_group(min: u64, max: u64) -> u64 {
     let mut k = min;
     let mut s: HashSet<u64> = HashSet::new();
 
-    loop {
+    while k <= max {
         if get_multiplication_partitions(q, k) {
             k += 1;
-            if k % 100 == 0 {
-                println!("at k = {}", k);
-            }
             s.insert(q);
-            q = min;
-        }
-
-        if k == max + 1 {
-            break;
+            q = k;
         }
 
         q += 1;
@@ -78,5 +73,4 @@ fn test_sum_group() {
     assert_eq!(sum_group(12000, 12000), 12096);
     assert_eq!(sum_group(2, 6), 30);
     assert_eq!(sum_group(2, 12), 61);
-    //assert_eq!(sum_group(2, 12000), 7587457);
 }
