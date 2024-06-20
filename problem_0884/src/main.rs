a3082e2fb5336d0b61067cd5062afedbff25a1c7
Gerard <gerard@wetransfer.com>
2024-06-20 11:11:10 +0200

    Amost there ...

--- problem_0884/src/main.rs
+++ problem_0884/src/main.rs
@@ -2,8 +2,42 @@ fn main() {
     println!("Answer: {}", detract_all(10_u128.pow(17)));
 }
 
-fn detract_all(n: u128) -> usize {
-    return detract(n)
+fn detract_all(n: u128) -> u128 {
+    let comb = all_cubes(n);
+    let mut abacus = vec![0; comb.len()];
+    let index = 0;
+    let mut steps = 0;
+
+    loop {
+        abacus[index] += 1;
+
+        let mut total = 0;
+        let mut j = 0;
+        for m in 0..comb.len() {
+            total += abacus[m] * comb[m];
+            if m + 1 < comb.len() && total >= comb[m + 1] {
+                j = m + 1;
+            }
+        }
+
+        for m in 0..j {
+            abacus[m] = 0;
+        }
+        if j > 0 {
+            abacus[j] += 1;
+        }
+
+        if total > n {
+            break;
+        }
+
+        for s in &abacus {
+            steps += s;
+        }
+        println!("{:?} {}", abacus, total);
+
+    }
+    return steps
 }
 
 #[test]
@@ -11,11 +45,17 @@ fn test_detract_all() {
     assert_eq!(detract_all(100), 512);
 }
 
-fn detract(_n: u128) -> usize {
-    return 0
-}
+fn all_cubes(n: u128) -> Vec<u128> {
+    let mut results = vec![];
+    let mut m: u128 = 1;
+    loop {
+        let p = m.pow(3);
+        if p > n {
+            break;
+        }
 
-#[test]
-fn test_detract() {
-    assert_eq!(detract(100), 4);
+        results.push(p);
+        m += 1;
+    }
+    return results;
 }
